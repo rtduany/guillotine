0b47178eedb81fe1ed94166eb65568defbe23ee4
rtduany <rtduany@gmail.com>
2015-11-18 03:27:21 +0300

    add guillotine

---
+++ app/src/main/java/com/yalantis/guillotine/sample/activity/MainActivity.java
@@ -0,0 +1,80 @@
+package com.yalantis.guillotine.sample.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import com.yalantis.guillotine.animation.GuillotineAnimation;
+import com.yalantis.guillotine.sample.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * Created by Dmytro Denysenko on 5/4/15.
+ */
+public class MainActivity extends AppCompatActivity {
+    private static final long RIPPLE_DURATION = 250;
+
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+    @InjectView(R.id.root)
+    FrameLayout root;
+    @InjectView(R.id.content_hamburger)
+    View contentHamburger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+        ButterKnife.inject(this);
+
+//if there's a toolbar
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle(null);
+        }
+
+        View guillotineMenu = LayoutInflater.from(this).inflate(R.layout.guillotine, null);
+        root.addView(guillotineMenu);
+
+        new GuillotineAnimation.GuillotineBuilder(guillotineMenu, guillotineMenu.findViewById(R.id.guillotine_hamburger), contentHamburger)
+                .setStartDelay(RIPPLE_DURATION)
+                .setActionBarViewForAnimation(toolbar)
+                .setClosedOnStart(true)
+                .build();
+
+
+        LinearLayout activityLayout = (LinearLayout) guillotineMenu.findViewById(R.id.activity_group);
+        activityLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+    public void settingsGroup(View view){
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+
+    }
+
+    public void profileGroup(View view){
+        Intent i = new Intent(this, Profile.class);
+        startActivity(i);
+    }
+
+    public void feedGroup(View v){
+        Intent i = new Intent(this, Feed.class);
+        startActivity(i);
+    }
+
+}
